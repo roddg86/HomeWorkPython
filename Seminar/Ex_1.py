5e59b47ad84bb03ae664feb47edd39678e6d1fcb
Dmitry <roddg86@gmail.com>
2022-12-17 04:41:31 +0900

    task 4 solved, add seminars

--- Seminar/Ex_1.py
+++ Seminar/Ex_1.py
@@ -9,11 +9,13 @@
 
 """ a = int(input("Введите первое число "))
 b = int(input("Введите второе число "))
-
-if a == b**2 or b == a**2:
+ """
+""" if a == b**2 or b == a**2:
     print('ДА')
 else:
     print("НЕТ") """
+
+# print(a ** 2 == b or b ** 2 == a) # второе решение
 
 """ 2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
 
@@ -26,6 +28,42 @@ else:
     input()), int(input()), int(input()), int(input())
 
 print(f'Максимальное число: {max(a,b,c,d,e)}') """
+
+""" list = []
+for i in range(5):
+    list.append(int(input(f'Введите {i + 1} число: ')))
+
+max = list[0]
+for i in list:
+    if i >= max:
+        max = i
+print(f'Максимальное число  {max}')
+ """
+
+""" def find_max(lst_nums):
+    max_num = lst_nums[0]
+
+    for number in lst_nums:
+        if number > max_num:
+            max_num = number
+        
+    return max_num
+
+lst_in = []
+
+number_els = int(input('Введите количество элементов: '))
+
+for i in range(number_els):
+    lst_in.append(int(input(f'Введите {i+1} элемент: ')))
+
+print(find_max(lst_in))
+ """
+""" list = []
+
+for i in range(0,5):
+    list.append(int(input(f'Введите {i + 1} элемент: ')))
+
+print(max(list)) """
 
 """ 3. С клавиатуры введено трехзначное число найти сумму его цифр """
 
@@ -45,3 +83,60 @@ if b != 0:
         print('НЕТ')
 else:
     print('На ноль делить нельзя') """
+
+""" 5. Напишите программу, которая будет на вход принимать 
+число N и выводить числа от -N до N
+
+*Примеры:*
+
+- 5 -> -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
+ """
+""" 
+a = abs(int(input("Введите число N ")))
+
+# for i in range(-a, a+1): print(i, end=' ')
+
+# второе решение
+lst_nums = list(range(-a, a + 1)) 
+print(*lst_nums, sep=', ') """
+
+""" 6. Напишите программу, которая будет принимать на вход 
+дробь и показывать первую цифру дробной части числа.
+
+*Примеры:*
+
+- 6,78 -> 7
+- 5 -> нет
+- 0,34 -> 3     """
+
+""" from math import *
+
+a = float(input("Введите число: "))
+
+if a - floor(a) != 0:
+    print(floor(a*10) % 10)
+else:
+    print('Дробной части нет!!!!!')
+ """
+
+""" 
+# через приведение типа
+
+from math import *
+
+a = float(input("Введите число: "))
+
+if a - int(a) != 0:
+    print(int(a*10) % 10)
+else:
+    print('Дробной части нет!!!!!')
+ """
+
+# еще одно решение
+number = float(input('Введите число: '))
+
+if number % 1 == 0:
+    print('Нет')
+else:
+    digit = number * 10 % 10
+    print(int(digit))
