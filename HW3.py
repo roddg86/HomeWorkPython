6dfdbf9c24dce3ad75ca81e95ff221a0dc5a458e
Dmitry <roddg86@gmail.com>
2022-12-24 11:59:05 +0900

    task 2,3 solved

--- HW3.py
+++ HW3.py
@@ -1,10 +1,11 @@
-""" 1. Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+""" 1. Задайте список из нескольких чисел. Напишите программу, 
+которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 
 Пример:
 
 - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12 """
 
-""" функция задает список из нескольких чисел """
+# """ функция задает список из нескольких чисел """
 
 
 # from random import randint
@@ -29,18 +30,80 @@
 # print(f'Сумма чисел на нечетных позициях {result}')
 
 
-""" 2. Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+""" 2. Напишите программу, которая найдёт произведение пар чисел списка. 
+Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
 Пример:
 
 - [2, 3, 4, 5, 6] => [12, 15, 16];
 - [2, 3, 5, 6] => [12, 15] """
 
-""" 3. Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+# from random import randint
+
+# def gen_list(number):
+#     list = []
+#     for i in range(number):
+#         list.append(randint(1, 9))
+#     return list
+
+# def product_pairs(list):
+#     list2 = []
+#     i = 0
+#     number = len(list)
+#     while i < len(list)//2 + len(list) % 2:
+#         number = number - 1
+#         a = list[i] * list[number]
+#         list2.append(a)
+#         i += 1
+#     return list2
+
+# number = int(input('Введите размер списка: '))
+# list = gen_list(number)
+# print(list)
+# print(product_pairs(list))
+
+
+""" 3. Задайте список из вещественных чисел. 
+Напишите программу, которая найдёт разницу между максимальным и минимальным значением
+дробной части элементов.
 
 Пример:
 
 - [1.1, 1.2, 3.1, 5, 10.01] => 0.19 """
+
+
+# from random import randint, uniform
+# def generate_list(number):
+
+#     my_list = []
+
+#     for _ in range(number):
+#         fract_part = randint(1, 2)
+#         my_list.append(round(uniform(1, 10), fract_part))
+#     return my_list
+
+
+# def difference(my_list):
+#     min = 1
+#     max = 0
+#     max_min = 0
+
+#     for i in my_list:
+#         if (i - int(i)) <= min:
+#             min = i - int(i)
+#         if (i - int(i)) >= max:
+#             max = i - int(i)
+#     max_min = round((max - min), 2)
+#     return max_min
+
+
+# number = int(input('Введите число: '))
+# my_list = generate_list(number)
+# #my_list = [1.1, 1.2, 3.1, 10.01]
+# print(f'Исходный список: {my_list}')
+# print(difference(my_list))
+
 
 """ 4. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 
@@ -70,15 +133,15 @@
 - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, -3, 2, -1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] """
 
 
-def fibonacci(number):
+# def fibonacci(number):
 
-    list = [0, 1]
-    for i in range(2, number + 1):
-        list.append(list[-1] + list[-2])
-    for i in range(number):
-        list = [list[1] - list[0]] + list
-    return list
+#     list = [0, 1]
+#     for i in range(2, number + 1):
+#         list.append(list[-1] + list[-2])
+#     for i in range(number):
+#         list = [list[1] - list[0]] + list
+#     return list
 
 
-number = int(input("Введите  число:\n"))
-print(f'для k = {number} список будет выглядеть так: {fibonacci(number)}')
+# number = int(input("Введите  число:\n"))
+# print(f'для k = {number} список будет выглядеть так: {fibonacci(number)}')
