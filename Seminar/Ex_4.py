3c543f79ce0e86950a404b84051e5adfe720113e
roddg <roddg@mail.ru>
2022-12-25 18:41:08 +0900

    task 3 evklid fix

--- Seminar/Ex_4.py
+++ Seminar/Ex_4.py
@@ -45,3 +45,14 @@
 #         if nok % n1 == 0 and nok % n2 == 0:
 #             break
 #     print(nok)
+
+
+# по алгоритму евклида
+
+# a, b = map(int, input('Введите два числа через пробел ').split())
+# s = a * b
+# a, b = max(a, b), min(a, b)
+# while a != 0 and b != 0:
+#     a, b = b, a % b
+# print(f'НОД = {b + a}')
+# print(f'НОК = {s // (b + a)}')
