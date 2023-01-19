eadfa9545c46967341b77e83539c1af468291383
roddg <roddg@mail.ru>
2023-01-19 18:18:42 +0900

    lecture 4 solved

---
+++ Lectures/lec4/calc/model_mult.py
@@ -0,0 +1,15 @@
+x = 0
+y = 0
+
+""" Метод инициализации начальных значений """
+
+
+def init(a, b):
+    global x
+    global y
+    x = a
+    y = b
+
+
+def do_it():
+    return x * y
---
+++ Lectures/lec4/join_job/main.py
@@ -0,0 +1,10 @@
+""" Главный модуль """
+
+import html_creater as hc
+import xml_generator as xg
+import data_provider as dp
+
+print(hc.create())
+print(xg.create())
+
+hc.new_create(xg.new_create(dp.data_collection()))
